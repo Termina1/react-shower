ea3c58b46258da4a4035dce4cfe21d19a6561771
Termina1 <terminal2010@gmail.com>
2016-05-08 01:05:55 +0300

    add build command

--- bin/reshower.js
+++ bin/reshower.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 var path = require('path');
-global.workingPath = process.cwd();
+var exec = require('child_process').exec;
 
-require('../server.dev.js');
+var cmd = process.argv.slice(-1)[0]
+switch (cmd) {
+  case "build":
+    var stream = exec("node_modules/.bin/webpack --config webpack.prod.config.js --progress --colors -p && cp -R js/utils build/js");
+    stream.stdout.pipe(process.stdout);
+    stream.stderr.pipe(process.stderr);
+    break;
+  default:
+    require('../server.dev.js');
+    break;
+}
