f6d71af688efc7016b26544fdff62e8ec49b1281
Termina1 <terminal2010@gmail.com>
2016-05-08 01:15:25 +0300

    build task for reshower

--- bin/reshower.js
+++ bin/reshower.js
@@ -5,7 +5,10 @@ var exec = require('child_process').exec;
 var cmd = process.argv.slice(-1)[0]
 switch (cmd) {
   case "build":
-    var stream = exec("node_modules/.bin/webpack --config webpack.prod.config.js --progress --colors -p && cp -R js/utils build/js");
+    var confpath = path.join(__dirname, "..", "webpack.prod.config.js");
+    var utils = path.join(__dirname, "..", "js/utils");
+    var utilsDest = path.join(process.cwd(), "build/js");
+    var stream = exec("node_modules/.bin/webpack --config " + confpath + "  --progress --colors -p && cp -R " + utils + " " + utilsDest);
     stream.stdout.pipe(process.stdout);
     stream.stderr.pipe(process.stderr);
     break;
