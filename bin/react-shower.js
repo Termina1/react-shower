53340e2b412b691e4df64c7adf07d074abe7725d
Termina1 <terminal2010@gmail.com>
2016-05-22 21:24:41 +0300

    remove console log

--- bin/react-shower.js
+++ bin/react-shower.js
@@ -10,7 +10,6 @@ switch (cmd) {
     var utilsDest = path.join(process.cwd(), "build/js");
     var dest = process.argv[3];
     var stream = exec("node_modules/.bin/webpack --config " + confpath + "  --progress --colors -p && cp -R " + utils + " " + utilsDest, function(err) {
-      console.log(err, dest);
       if (!err && dest) {
         exec('mv ./build ' + dest);
       }
