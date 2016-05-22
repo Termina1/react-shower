56e0caeebd7997cc663ea2a582b8a9433b6dba87
Termina1 <terminal2010@gmail.com>
2016-05-22 20:36:21 +0300

    add some params for command build

--- bin/react-shower.js
+++ bin/react-shower.js
@@ -2,13 +2,19 @@
 var path = require('path');
 var exec = require('child_process').exec;
 
-var cmd = process.argv.slice(-1)[0]
+var cmd = process.argv[2]
 switch (cmd) {
   case "build":
     var confpath = path.join(__dirname, "..", "webpack.prod.config.js");
     var utils = path.join(__dirname, "..", "js/utils");
     var utilsDest = path.join(process.cwd(), "build/js");
-    var stream = exec("node_modules/.bin/webpack --config " + confpath + "  --progress --colors -p && cp -R " + utils + " " + utilsDest);
+    var dest = process.argv[3];
+    var stream = exec("node_modules/.bin/webpack --config " + confpath + "  --progress --colors -p && cp -R " + utils + " " + utilsDest, function(err) {
+      console.log(err, dest);
+      if (!err && dest) {
+        exec('mv ./build ' + dest);
+      }
+    });
     stream.stdout.pipe(process.stdout);
     stream.stderr.pipe(process.stderr);
     break;
